1ddea26d1041123c397f6c9638805e5d1a3840a6
kramer1000 <dllerena89@gmail.com>
2022-12-27 20:49:57 -0500

    i added my first line

--- .idea/src/test.java
+++ .idea/src/test.java
@@ -5,3 +5,10 @@ this is my second line
 I added this line on github
 
 Another line
+
+commit -> save my changes to my local repo
+push -> sending your committed changes to git
+
+
+if ou want to get the latest code from server
+Git -> pull
